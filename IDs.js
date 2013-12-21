79619e4e248613d540cbcfb3bd93d569403ac353
Akari1 <sovanven01@gmail.com>
2013-12-21 13:10:21 -0600

    Create IDs.js

---
+++ IDs.js
@@ -0,0 +1,6 @@
+({
+onPlayerReceived: function(id){
+client.printLine(id + ' - ' + client.name(id)); //DO NOT USE ON BIG SERVERS OR THIS WILL GO OA AND STUFF I gave you a warning already
+return;
+}
+});
