3feea1bf7fc2b6aedaeba35e6dccc000dc19ebb2
Akari1 <sovanven01@gmail.com>
2013-12-21 14:58:24 -0600

    Warning

--- pjpm.js
+++ pjpm.js
@@ -1,4 +1,4 @@
-({
+({ // DO NOT USE ON BIG SERVERS LOL
 onPlayerReceived: function(id){
 client.network().sendPM(id, "SCRIPT TEST"); // Chnage the message, this script is as Paladin says, (13:06) Paladin: faggy
 return;
